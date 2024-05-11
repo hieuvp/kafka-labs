44405d960e8b7937c91ff0d0e30ea9caf3b8efa8
Hieu Van <hieu.van@personetics.com>
2024-05-11 22:46:17 +0700

    docker-compose.yml

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -17,5 +17,6 @@ prettier --write ./*.yml
   terraform fmt
   prettier --write ./*.yml
 
+  grep "^[A-Za-z]" "secrets.yml" | sed -E "s/([A-Za-z_]+):(\s+)(.+)/\1: \"<YOUR_\U\1>\"/" > "secrets.yml.example"
   grep "^[A-Za-z]" "terraform.tfvars" | sed -E "s/([A-Za-z_]+)(\s+)=(\s+)(.+)/\1 = \"<YOUR_\U\1>\"/" > "terraform.tfvars.example"
 )
